289fa545be64af1111df14e239b2073f2fa9f40b
Dillon Lee <deeleeTech@outlook.com>
2022-01-28 20:50:17 -0600

    api now pulls from database

--- routes/teamData.js
+++ routes/teamData.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+// Requiring the module
+const reader = require('xlsx')
 
-/* GET team data. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET ALL TEAMS  */
+router.get('/getAll', function(req, res, next) {
+  // Reading our test file
+const file = reader.readFile(`${__dirname}/teams.xlsx`)
+const sheets = file.SheetNames;
+ 
+let allTeams = [];
+sheets.map((eachSheet, index)=>{
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[index]]);
+    temp.forEach((res) => {
+      allTeams.push(res)
+   })
+})
+  res.send(allTeams); // CLIENT SENDOFF ---->
 });
 
 module.exports = router;
--- scripts/test.js
+++ scripts/test.js
@@ -4,30 +4,15 @@ const reader = require('xlsx')
   
 // Reading our test file
 const file = reader.readFile('../database/teams.xlsx')
-  
-let data = []
-  
 const sheets = file.SheetNames
  
-//makes TEAMS array
-for(let i = 0; i < sheets.length; i++)
-{
-   const temp = reader.utils.sheet_to_json(
-        file.Sheets[file.SheetNames[i]])
-   temp.forEach((res) => {
-      data.push(res)
+let allTeams = [];
+  sheets.map((eachSheet, index)=>{
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[index]]);
+    temp.forEach((res) => {
+      allTeams.push(res)
    })
-}
-
-function getTeamID(teamNickname){
-   let teamIDnumber;
-   data.map((eachTeam)=>{
-      if(eachTeam.NICKNAME == teamNickname){
-         teamIDnumber = eachTeam.TEAM_ID;
-      }
-   })
-   return teamIDnumber
-}
+  })
   
 // Printing data
-console.log(data)
+console.log(allTeams)
